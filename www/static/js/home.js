4554e60da6cad706256235d8a19a226729a96b13
K.Lee <bklitong@gmail.com>
2017-10-12 19:30:53 +0800

    add wwwfile

---
+++ www/static/js/home.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: K
+ * @Date:   2017-10-10 16:22:25
+ * @Last Modified by:   K
+ * @Last Modified time: 2017-10-11 10:48:32
+ */
+
+
+function sayHello() {
+    alert(`Hi
+		   这是一个
+		   多行字符
+		   测试`);
+}
